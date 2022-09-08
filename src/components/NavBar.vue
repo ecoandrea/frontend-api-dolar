7095bf3578f9922825d0d5478b6e17a2566458d0
EngelReyes23 <engelreyes23@gmail.com>
2022-09-08 16:51:57 -0600

    diseño Navbar, toggle con iconos, y darkmode :D

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,34 @@
-<script setup></script>
+<script setup>
+import IconDarkMode from '@/components/icons/IconDarkMode.vue';
+import IconLightMode from '@/components/icons/IconLightMode.vue';
+import { ref } from 'vue';
+
+// INFO: Nunca he usado Vue xD, soy mas de React
+
+// Verifica si el tema del SO es DarkMode
+const isDarkMode = ref(window.matchMedia('(prefers-color-scheme: dark)').matches)
+
+const toggleDarkMode = () => {
+  isDarkMode.value = !isDarkMode.value
+  isDarkMode.value ? document.body.classList.add('dark') : document.body.classList.remove('dark')
+}
+</script>
+
 <template>
-    <nav class="text-gray-100 bg-gray-700">
-        <div
-            class="container flex items-center justify-between px-4 py-4 mx-auto"
-        >
-            <p>Logo API Dolar</p>
-            <div class="flex items-center gap-2">
-                <span
-                    class="text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >Dark/Light Mode</span
-                >
-                <label
-                    for="default-toggle"
-                    class="relative inline-flex items-center cursor-pointer"
-                >
-                    <input
-                        type="checkbox"
-                        value=""
-                        id="default-toggle"
-                        class="sr-only peer"
-                    />
-                    <div
-                        class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-gray-300 dark:peer-focus:ring-gray-800 dark:bg-gray-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-800"
-                    ></div>
-                </label>
-            </div>
-        </div>
-    </nav>
+  <header class="bg-gray-800 text-white p-4 mb-5">
+    <div class="max-w-7xl flex justify-between items-center mx-auto">
+      <h1
+        class="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500"
+      >
+        Logo API Dollar
+      </h1>
+      <button
+        @click="toggleDarkMode"
+        class="active:ring-rose-400 hover:ring rounded-lg p-0.5 outline-none ring-indigo-500 dark:text-white"
+      >
+        <IconDarkMode v-if="isDarkMode" />
+        <IconLightMode v-else />
+      </button>
+    </div>
+  </header>
 </template>
